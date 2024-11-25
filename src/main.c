7b708288ade0ff4c4069f2626f919d571662a2f1
kaan <kaandesu00@gmail.com>
2024-11-25 22:40:55 +0100

    chore: refactor and add recv

--- src/main.c
+++ src/main.c
@@ -9,67 +9,60 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-#define BUFFER_SIZE 2048
 #define PORT 3000
+#define BUFFER_SIZE 2048
+#define BACKLOG 5
+
+void check(int e, const char *context) {
+  if (e < 0) {
+    printf("%s failed: %s", context, strerror(errno));
+    exit(EXIT_FAILURE);
+  }
+}
 
 int main(void) {
-  int server_fd;
-  if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-    printf("Could not start the server socket: %s", strerror(errno));
-    exit(EXIT_FAILURE);
-  }
-  int reuse = 1;
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse))) {
-    printf("Could not re-use the port: %s", strerror(errno));
-    exit(EXIT_FAILURE);
-  }
+  int server_fd, client_fd;
+  struct sockaddr_in client_addr;
   struct sockaddr_in server_addr = {
       .sin_family = AF_INET,
       .sin_port = htons(PORT),
       .sin_addr = {htonl(INADDR_ANY)},
   };
-
-  if (bind(server_fd, (const struct sockaddr *)&server_addr,
-           sizeof(server_addr)) < 0) {
-    printf("Bind failed: %s", strerror(errno));
-    exit(EXIT_FAILURE);
-  }
-
-  int8_t backlogCount = 5;
-  if (listen(server_fd, backlogCount) < 0) {
-    printf("Listen failed: %s", strerror(errno));
-    exit(EXIT_FAILURE);
-  }
-
-  struct sockaddr_in client_addr;
   socklen_t client_addr_len = sizeof(client_addr);
+
+  server_fd = socket(AF_INET, SOCK_STREAM, 0);
+  check(server_fd, "socket creation");
+
+  int reuse = 1;
+  check(setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse)),
+        "setting up socket option");
+
+  check(bind(server_fd, (const struct sockaddr *)&server_addr,
+             sizeof(server_addr)),
+        "server bind");
+
+  check(listen(server_fd, BACKLOG), "listen server");
+  client_fd =
+      accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+  check(client_fd, "accept");
+
   uint8_t buffer[BUFFER_SIZE];
   memset(buffer, 0, BUFFER_SIZE);
-
-  printf("Listening on port :%d...\n", PORT);
-  int client_fd =
-      accept(server_fd, (struct sockaddr *)&client_addr, &client_addr_len);
-  if (client_fd < 0) {
-    printf("Accept failed: %s", strerror(errno));
-    exit(EXIT_FAILURE);
-  }
+  check(recv(client_fd, buffer, BUFFER_SIZE, 0), "recive stuff");
 
   for (int i = 0; i < BUFFER_SIZE; i++) {
-    if (buffer[i] != 255) {
-      printf("%c", buffer[i]);
-    }
+    printf("%c", buffer[i]);
   }
-
-  printf("client connected \n");
 
   char *res = "HTTP/1.1 200 OK\r\n"
               "Content-Length: 13\r\n"
-              "Connection: close\r\n"
+              "Connection: keep-alive\r\n"
               "\r\n"
               "Hello, world!";
+
   send(client_fd, res, strlen(res), 0);
 
   close(client_fd);
   close(server_fd);
-  return EXIT_SUCCESS;
+  exit(EXIT_SUCCESS);
 }
